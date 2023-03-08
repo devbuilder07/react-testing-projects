cd7e7f325dd1c55ac13d08a4c99bbad2b46380e9
unknown <devbuilder07@gmail.com>
2023-03-08 23:48:21 +0530

    card list

--- react-pro-function-2/src/components/card-list/cards-list.jsx
+++ react-pro-function-2/src/components/card-list/cards-list.jsx
@@ -1,22 +1,19 @@
-import { Component } from "react";
 import Card from "../card/card";
 import './cards-list.css'
-class CardList extends Component {
-    render() {
-        let { users } = this.props;
-        return (
-            <div className='card-list'>
-                {
-                    users.map((user) => {
-                        let { id } = user;
-                        return (
-                            <Card user={user} key={id} />
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+
+const CardList = ({ users }) => {
+    return (
+        <div className='card-list'>
+            {
+                users.map((user) => {
+                    let { id } = user;
+                    return (
+                        <Card user={user} key={id} />
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default CardList
